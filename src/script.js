fb3eede7ea4e40971712fdb580284aa0e0a2ff45
Gunnar Von Bergen <vonbergb@miamioh.edu>
2024-02-28 20:31:04 -0500

    Added potential attributes in readme, started character object template

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
 // Javascript will be used to provide the interactive elements of our site.
+const character = {
+    // basicInfo created as a way to encapsulate similar instance variables
+    // to access these, reference as character.basicInfo.name
+    basicInfo: {
+      name: "",
+      race: "",
+      class: "",
+      level: 0,
+      background: "",
+      alignment: "",
+      experiencePoints: 0,
+    }
+    // other properties...
+  };
