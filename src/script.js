ade5eb8476339c7e95877b7b252a42c390de2352
Gunnar Von Bergen <vonbergb@miamioh.edu>
2024-03-01 12:24:08 -0500

    uses key value pair to log form data

--- src/script.js
+++ src/script.js
@@ -6,44 +6,43 @@ function createCharacter(event) {
   const form = document.getElementById("characterInfoForm");
   const formData = new FormData(form);
 
-  console.log(formData.get("charName"));
-  console.log(formData.get("charRace"));
-  console.log(formData.get("charClass"));
-  console.log(formData.get("charLevel"));
-  console.log(formData.get("charDescription"));
-  console.log(formData.get("charAlignment"));
+  // Log each form field value
+  for (let [key, value] of formData.entries()) {
+    // ${var} is a template literal, which allows us to insert variables into a string
+    console.log(`${key}: ${value}`);
+  }
 }
 
 // character object all data a single character contains
 const character = {
-    // basicInfo created as a way to encapsulate similar instance variables
-    // to access these, reference as character.basicInfo.name
-    basicInfo: {
-      name: "",
-      race: "",
-      class: "",
-      level: 0,
-      background: "",
-      alignment: "",
-      experiencePoints: 0,
-    },
-    abilityScores: {
-      strength: "",
-      dexterity: "",
-      constitution: "",
-      intelligence: "",
-      wisdom: "",
-      charisma: "",
-    },
-    combat: {
-      armorClass: "",
-      initiative: "",
-      speed: "",
-      hitPointMaximum: "",
-      currentHitPoints: "",
-      temporaryHitPoints: "",
-      hitDice: "",
-      deathSaves: "",
-    }
-    // Still more properties are needed; refer to readme
-  };
+  // basicInfo created as a way to encapsulate similar instance variables
+  // to access these, reference as character.basicInfo.name
+  basicInfo: {
+    name: "",
+    race: "",
+    class: "",
+    level: 0,
+    background: "",
+    alignment: "",
+    experiencePoints: 0,
+  },
+  abilityScores: {
+    strength: "",
+    dexterity: "",
+    constitution: "",
+    intelligence: "",
+    wisdom: "",
+    charisma: "",
+  },
+  combat: {
+    armorClass: "",
+    initiative: "",
+    speed: "",
+    hitPointMaximum: "",
+    currentHitPoints: "",
+    temporaryHitPoints: "",
+    hitDice: "",
+    deathSaves: "",
+  }
+  // Still more properties are needed; refer to readme
+};
