9222d218bacc73174f73bcad5fc5d351824d88aa
Gunnar Von Bergen <vonbergb@miamioh.edu>
2024-03-01 12:17:21 -0500

    when submit  is pressed, log info to console

--- src/script.js
+++ src/script.js
@@ -1,4 +1,20 @@
 // Javascript will be used to provide the interactive elements of our site.
+
+function createCharacter(event) {
+  event.preventDefault();
+
+  const form = document.getElementById("characterInfoForm");
+  const formData = new FormData(form);
+
+  console.log(formData.get("charName"));
+  console.log(formData.get("charRace"));
+  console.log(formData.get("charClass"));
+  console.log(formData.get("charLevel"));
+  console.log(formData.get("charDescription"));
+  console.log(formData.get("charAlignment"));
+}
+
+// character object all data a single character contains
 const character = {
     // basicInfo created as a way to encapsulate similar instance variables
     // to access these, reference as character.basicInfo.name
