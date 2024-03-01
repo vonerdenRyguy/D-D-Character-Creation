1d52ee50d55f8563a03c636e0361ad4cb269ab4d
Gunnar Von Bergen <vonbergb@miamioh.edu>
2024-03-01 12:48:35 -0500

    updated names to keep js simpler

--- src/script.js
+++ src/script.js
@@ -8,41 +8,40 @@ function createCharacter(event) {
 
   // Log each form field value
   for (let [key, value] of formData.entries()) {
-    // ${var} is a template literal, which allows us to insert variables into a string
     console.log(`${key}: ${value}`);
   }
-}
 
-// character object all data a single character contains
-const character = {
-  // basicInfo created as a way to encapsulate similar instance variables
-  // to access these, reference as character.basicInfo.name
-  basicInfo: {
-    name: "",
-    race: "",
-    class: "",
-    level: 0,
-    background: "",
-    alignment: "",
-    experiencePoints: 0,
-  },
-  abilityScores: {
-    strength: "",
-    dexterity: "",
-    constitution: "",
-    intelligence: "",
-    wisdom: "",
-    charisma: "",
-  },
-  combat: {
-    armorClass: "",
-    initiative: "",
-    speed: "",
-    hitPointMaximum: "",
-    currentHitPoints: "",
-    temporaryHitPoints: "",
-    hitDice: "",
-    deathSaves: "",
-  }
-  // Still more properties are needed; refer to readme
-};
+  // character object all data a single character contains
+  const character = {
+    // basicInfo created as a way to encapsulate similar instance variables
+    // to access these, reference as character.basicInfo.name
+    basicInfo: {
+      name: "",
+      race: "",
+      class: "",
+      level: 0,
+      background: "",
+      alignment: "",
+      experiencePoints: 0,
+    },
+    abilityScores: {
+      strength: 0,
+      dexterity: 0,
+      constitution: 0,
+      intelligence: 0,
+      wisdom: 0,
+      charisma: 0,
+    },
+    combat: {
+      armorClass: "",
+      initiative: "",
+      speed: "",
+      hitPointMaximum: "",
+      currentHitPoints: "",
+      temporaryHitPoints: "",
+      hitDice: "",
+      deathSaves: "",
+    }
+    // Still more properties are needed; refer to readme
+  };
+}
